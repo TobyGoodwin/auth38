5ec5175f9bd9ca75b2a08721a8fec8599bff27c1
Toby Goodwin <toby@mythic-beasts.com>
2017-10-16 09:38:04 +0100

    types for dispatch

--- src/Yesod/Auth/Mythic.hs
+++ src/Yesod/Auth/Mythic.hs
@@ -1,5 +1,7 @@
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RankNTypes #-}
+
 -- | Provides a dummy authentication module that simply lets a user specify
 -- his/her identifier. This is not intended for real world use, just for
 -- testing.
@@ -7,22 +9,24 @@ module Yesod.Auth.Mythic
     ( authMythic
     ) where
 
+import Data.Text (Text)
 import Yesod.Auth
 import Yesod.Form (runInputPost, textField, ireq)
 import Yesod.Core
 
 authMythic :: YesodAuth m => AuthPlugin m
-authMythic =
-    AuthPlugin "mythic" dispatch login
-  where
-    dispatch "POST" [] = do
-        ident <- lift $ runInputPost $ ireq textField "ident"
-        lift $ setCredsRedirect $ Creds "mythic" ident []
-    dispatch _ _ = notFound
-    url = PluginR "mythic" []
-    login authToMaster = do
-        request <- getRequest
-        toWidget [hamlet|
+authMythic = AuthPlugin "mythic" dispatch login
+
+dispatch :: YesodAuth master =>
+              Text -> [Text] -> AuthHandler master TypedContent
+dispatch "POST" [] = do
+    ident <- lift $ runInputPost $ ireq textField "ident"
+    lift $ setCredsRedirect $ Creds "mythic" ident []
+dispatch _ _ = notFound
+
+login authToMaster = do
+    request <- getRequest
+    toWidget [hamlet|
 $newline never
 <form method="post" action="@{authToMaster url}">
     $maybe t <- reqToken request
@@ -31,3 +35,5 @@ $newline never
     <input type="text" name="ident">
     <input type="submit" value="Mythic Login">
 |]
+  where
+    url = PluginR "mythic" []
